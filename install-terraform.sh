2641ed3f24b80c94344708f3ab5b1712e4f6210e
Siddhant Khare <Siddhantkhare2694@gmail.com>
2023-01-07 15:20:48 +0530

    cleanup :sparkles:

--- install-terraform.sh
+++ install-terraform.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-if command -v /usr/bin/terraform &>/dev/null; then
+if command -v /usr/local/bin/terraform &>/dev/null; then
     echo "Terraform is already installed"
     exit
 fi
 
 # Get the stable version of Terraform
 TF_VERSION=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].builds[].version' | egrep -v 'rc|beta|alpha' | tail -1)
-
-if [[ -z "${TF_VERSION}" ]]; then
-  echo `Installing Terraform version ${TF_VERSION}`
-fi
 
 echo "Setting-up Terraform"
 OLD_DIR="$PWD"
@@ -19,6 +15,7 @@ cd "${TMP_DIR}" || exit 1
 
 curl -LO "https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip"
 unzip terraform_${TF_VERSION}_linux_amd64.zip
+sudo mv -f terraform /usr/local/bin/
 rm terraform_${TF_VERSION}_linux_amd64.zip
 
 cd "${OLD_DIR}" || exit 1
