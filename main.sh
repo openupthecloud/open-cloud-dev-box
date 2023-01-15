75a506c0e61a2b1fb0043fad77e78596df4154ab
Lou Bichard <contact@louisjohnbichard.co.uk>
2023-01-15 18:13:54 +0000

    fix: adding basic devcontainer

--- main.sh
+++ main.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-source ./setup-env-variables.sh
-./install-aws-cli.sh
-./collect-credentials.sh
-./open-cloud-dashboards.sh
+source ./install-terraform.sh
+source ./install-aws-cli.sh
 source ./install-kubectl.sh
 source ./install-terraform.sh
+source ./install-kubectl.sh
+source ./install-vsextensions.sh
+
+source ./setup-env-variables.sh
+source ./collect-credentials.sh
+source ./open-cloud-dashboards.sh
