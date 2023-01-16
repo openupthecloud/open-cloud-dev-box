54cfee6d6f7083eeedf46c8108f13b6a3e315e40
YAYA <yahyaabulhaj@outlook.com>
2023-01-16 23:15:28 +0100

    GCP

--- install-gcloud.sh
+++ install-gcloud.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Install dependencies
+gp sync-await apt-get-lock
 sudo apt-get install apt-transport-https ca-certificates gnupg -y
+gp sync-done apt-get-lock
 
 # Add the GCP package repository
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
@@ -10,7 +12,8 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.clou
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 
 # Update package list and install the Cloud SDK
+gp sync-await apt-get-lock
 sudo apt-get update && sudo apt-get install google-cloud-cli -y
+gp sync-done apt-get-lock
 
 echo "Google Cloud SDK installed successfully!"
-
