6275ae9eea3503e3cb6e640523ed2fa488740663
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2023-02-01 20:29:49 +0200

    change installation method of aws-vault
    didn't want to install brew on docker image, so this is easier method.

--- setup-env-variables.sh
+++ setup-env-variables.sh
@@ -3,4 +3,10 @@
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_VAULT_BACKEND=file
 export AWS_VAULT_FILE_PASSPHRASE=
-brew install aws-vault
+
+# Download & Install aws-vault
+curl -L -o /usr/local/bin/aws-vault https://github.com/99designs/aws-vault/releases/download/v6.6.2/aws-vault-linux-amd64
+chmod 755 /usr/local/bin/aws-vault
+
+# show aws-vault version
+aws-vault --version
